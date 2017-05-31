3f2dca4ae51bc8e3f6146427b0ee2699a5caad96
Lana Chiad <lanachiad7@gmail.com>
2017-05-30 21:40:58 -0400

    Create category new and create routes and functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,9 +7,17 @@ class CategoriesController < ApplicationController
   end
 
   def new
+  	@category = Category.new
   end
 
   def create
+  	@category = Category.new(category_params)
+  	if @category.save
+  		redirect_to @category 
+  	else
+  		@errors = category.errors.full_messages
+  		render :new
+  	end
   end
 
   def update
@@ -19,5 +27,10 @@ class CategoriesController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def category_params
+  	params.require(:category).permit(:title)
   end
 end

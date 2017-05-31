5c59be83b7001822b6179f44c254097e6986ad97
Lana Chiad <lanachiad7@gmail.com>
2017-05-30 21:28:04 -0400

    Create categories/index route, controller, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'categories/index'
+  resources :categories
+  root 'categories#index'
 end

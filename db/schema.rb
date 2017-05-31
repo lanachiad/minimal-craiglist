60a59c0e46e847b67aa49cc3e0a770171e77eb8b
Lana Chiad <lanachiad7@gmail.com>
2017-05-30 21:07:19 -0400

    Update Article to include reference to Category

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170531002653) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|

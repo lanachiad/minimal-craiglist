c271f4d0415118ede528e7f5721b85f5d79479c1
Lana Chiad <lanachiad7@gmail.com>
2017-05-30 21:33:54 -0400

    Create CRUD routes for Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,16 @@ class CategoriesController < ApplicationController
 
   def new
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   get 'categories/index'
-  resources :categories
-  root 'categories#index'
+
+  get 'categories/show'
+
+  get 'categories/create'
+
+  get 'categories/update'
+
+  get 'categories/destroy'
+
+  get 'categories/edit'
+
+  get 'categories/new'
+
+  resources :categories, only: [:index, :show, :new, :create] do
+    resources :articles, only: [:show, :new, :create]
+  end
 end
